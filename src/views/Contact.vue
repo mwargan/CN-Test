70750bf35913c2253124a1267041d9f47fb3b4ac
Michał Wargan <mwargan@gmail.com>
2021-03-16 22:34:14 +0100

    changed routing mode to history

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,14 +2,14 @@
 <div class="contact">
     <h1>Get in touch</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-    <form class="card">
+    <form class="card" method="post">
         <div class="card-heading">Contact Form</div>
         <div class="grid-columns">
             <label>Name <span class="required-attribute">*</span>
-              <input type="text" name="" value="" v-model="searchTerm">
+              <input type="text" name="" value="">
             </label>
             <label>Email Address <span class="required-attribute">*</span>
-              <input type="text" name="" value="" v-model="searchTerm">
+              <input type="text" name="" value="">
             </label>
         </div>
         <label>Message <span class="required-attribute">*</span>
